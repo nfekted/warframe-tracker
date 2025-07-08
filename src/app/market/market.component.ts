315bdc535b31e31573d5057a3651daaac5c24b3c
Leandro Souto de Paula <le-soutopaula@hotmail.com>
2025-07-07 22:04:36 -0300

    Price data

--- src/app/market/market.component.ts
+++ src/app/market/market.component.ts
@@ -6,8 +6,7 @@ import { Account } from '../../shared/models/account.model';
 import { PythonToolComponent } from '../python-tool/python-tool.component';
 import itemEnJson from '../../shared/jsons/tradeable/tradeEn.json';
 import itemPtJson from '../../shared/jsons/tradeable/tradePt.json'
-import Swal from 'sweetalert2';
-import { firstValueFrom, timer } from 'rxjs';
+import itemPrices from '../../shared/jsons/tradeable/tradePrices.json';
 
 @Component({
   selector: 'app-market',
@@ -23,16 +22,22 @@ export class MarketComponent {
 
   @Output() update = new EventEmitter<void>();
 
-  tradeableList: { url: string, item_en: string, item_pt: string }[] = [];
+  tradeableList: { id: string, url: string, item_en: string, item_pt: string }[] = [];
 
   text: string;
 
-  tradeItem: { url: string, name: string, lowest1: number, lowest2: number, lowest3: number, online1: number, online2: number, online3: number }[] = [];
+  tradeItem: { id: string, url: string, name: string, lowest: number, higher: number, average: number, most_frequent: number }[] = [];
   complete: boolean = false;
   current: number = 0;
   currentItem: string = '';
 
   lastSort: string = '';
+
+  enableScan: boolean = false;
+
+  searchTerm: string = '';
+  filteredItems = [];
+  myList: { id: string, url: string, name: string, lowest: number, higher: number, average: number, most_frequent: number }[] = [];
 
   constructor(private service: ApiService) { }
 
@@ -42,120 +47,160 @@ export class MarketComponent {
 
   makeJson() {
     this.tradeableList = [];
-    const itemEn = new Map<string, string>();
-    const itemPt = new Map<string, string>();
-    itemEnJson.payload.items.forEach((item) => itemEn.set(item.url_name, item.item_name.replace(':', '')));
-    itemPtJson.payload.items.forEach((item) => itemPt.set(item.url_name, item.item_name.replace(':', '')));
+
+    const itemEn = new Map<string, { id: string, name: string }>();
+    const itemPt = new Map<string, { id: string, name: string }>();
+    itemEnJson.payload.items.forEach((item) => itemEn.set(item.url_name, { id: item.id, name: item.item_name.replace(':', '') }));
+    itemPtJson.payload.items.forEach((item) => itemPt.set(item.url_name, { id: item.id, name: item.item_name.replace(':', '') }));
 
 
     itemEn.forEach((item, url) => {
       const pt = itemPt.get(url);
-      this.tradeableList.push({ url: url, item_en: item, item_pt: pt });
+      this.tradeableList.push({ id: item.id, url: url, item_en: item.name, item_pt: pt.name });
     });
+  }
+
+  onSearchChange(event: any) {
+    this.searchTerm = event.target.value;
+    this.filterItems();
+  }
+
+  filterItems() {
+    if (!this.searchTerm) {
+      this.filteredItems = [];
+      return;
+    }
+    const filterValue = this.searchTerm.toLowerCase();
+    this.filteredItems = this.tradeableList.filter(item =>
+      item.item_en.toLowerCase().includes(filterValue) || item.item_pt.toLocaleLowerCase().includes(filterValue)
+    );
+  }
+
+  selectItem(item) {
+    this.searchTerm = '';
+    this.filteredItems = [];
+    if (this.myList.find(i => i.id == item.id)) return;
+
+    const result = itemPrices.find(i => i.id == item.id);
+    if (result) this.myList.unshift(itemPrices.find(i => i.id == item.id));
   }
 
   scan() {
-    this.startScan();
-    // Swal.fire({
-    //   title: 'Pay attention!',
-    //   html: 'Due to security, requests to the Warframe Market API will be limited to 2 or 3 per second, without considering the API response time.<br/>'
-    //     + 'For every 100 items, it will take at least 30 seconds, and may extend for more than 3 minutes...<br/>'
-    //     + '<b>Misuse of the tool</b> can block your IP from the warframe market.',
-    //   icon: `warning`,
-    //   confirmButtonText: `Continue`,
-    //   cancelButtonText: 'Cancel',
-    //   showCancelButton: true
-    // }).then((res) => {
-    //   if (res.isConfirmed) {
-    //     this.startScan();
-    //   }
-    // })
+    this.formatPriceJson();
+    // this.getPrices();
   }
 
-  startScan() {
-    this.complete = false;
-    this.current = 1;
-    Swal.fire({
-      title: 'Loading',
-      text: 'Load and reading the images may take some time, check the tool console for progress information.',
-      showCancelButton: false,
-      showConfirmButton: false,
-      allowOutsideClick: false,
-    });
-    this.service.scanMarket().subscribe({
-      next: (res) => {
-        Swal.close();
-        this.text = this.format(res);
+  formatPriceJson() {
+    for (const key of Object.keys(itemPrices)) {
+       this.current += 1;
+      const value = itemPrices[key];
+      let item = value.filter(i => i.order_type == 'sell');
+      if (item.length > 1) item = item.filter(i => i.mod_rank == 0);
 
-        timer(10).subscribe(async () => {
-          for (const item of this.tradeableList) {
-            if (this.text.includes(this.format(item.item_en))) {
-              this.tradeItem.push({ url: item.url, name: item.item_en, lowest1: 0, lowest2: 0, lowest3: 0, online1: 0, online2: 0, online3: 0 });
-              this.text = this.text.replace(item.item_en.toUpperCase(), '');
-            } else if (this.text.includes(this.format(item.item_pt))) {
-              this.tradeItem.push({ url: item.url, name: item.item_pt, lowest1: 0, lowest2: 0, lowest3: 0, online1: 0, online2: 0, online3: 0 });
-              this.text = this.text.replace(item.item_pt.toUpperCase(), '');
-            }
-          }
-
-          for (let i = 0; i < this.tradeItem.length; i++) {
-            this.currentItem = this.tradeItem[i].name;
-            let response = await firstValueFrom(this.service.getMarketPrices(this.tradeItem[i].url));
-            if (!response?.payload?.orders) { this.current += 1; continue; }
-
-            const limit: Date = new Date();
-            limit.setMonth(new Date().getMonth() - 3);
-
-            response = response?.payload?.orders as Array<any>;
-            response = response.filter(i => i.visible && i.order_type == "sell" && new Date(i.user.last_seen) >= limit);
-
-            const onlines = response.filter(i => i.user.status == 'ingame');
-            const offlines = response.filter(i => i.user.status == 'offline');
-
-            onlines.sort((a, b) => { return +a.platinum - +b.platinum });
-            offlines.sort((a, b) => { return +a.platinum - +b.platinum });
-
-            this.tradeItem[i].lowest1 = offlines[0]?.platinum ? offlines[0].platinum : 0;
-            this.tradeItem[i].lowest2 = offlines[1]?.platinum ? offlines[1].platinum : 0;
-            this.tradeItem[i].lowest3 = offlines[2]?.platinum ? offlines[2].platinum : 0;
-
-            this.tradeItem[i].online1 = onlines[0]?.platinum ? onlines[0].platinum : 0;
-            this.tradeItem[i].online2 = onlines[1]?.platinum ? onlines[1].platinum : 0;
-            this.tradeItem[i].online3 = onlines[2]?.platinum ? onlines[2].platinum : 0;
-
-            this.current += 1;
-          }
-
-          this.complete = true;
+      if (item.length == 1) {
+        item = item[0];
+        const info = this.tradeableList.find(i => i.id == item.item_id);
+        this.tradeItem.push({
+          id: item.item_id,
+          url: info.url,
+          name: info.item_en,
+          lowest: +item.min_price,
+          higher: +item.max_price,
+          average: +item.avg_price.toFixed(0),
+          most_frequent: +item.median
         });
-      },
-      error: (e) => {
-        if (e.status == 0) {
-          Swal.fire('API not found', 'Api is not online, see the information on the page to configure it', 'error');
-          return;
-        }
-
-        Swal.fire('Error', e.message, 'error');
       }
-    });
+    }
+
+    this.downloadText();
+
   }
 
-  private format(text: string): string {
-    if (text == null) return '';
+  //Old crawler for prices
+  // async getPrices() {
+  //   if (this.tradeItem.length > 0) {
+  //     this.downloadText();
+  //     return;
+  //   }
 
-    text = text.split('\n').join('');
-    text = text.split('|').join('');
-    text = text.split(':').join('');
-    text = text.split('”').join('').split('“').join('');
-    text = text.split('‘').join('').split('’').join('');
-    text = text.split(' ').join('');
-    text = text.toUpperCase();
+  //   this.tradeItem = this.tradeableList.map(item => ({
+  //     id: item.id,
+  //     url: item.url,
+  //     name: item.item_en,
+  //     lowest: 0,
+  //     higher: 0,
+  //     average: 0,
+  //     most_frequent: 0
+  //   }));
 
-    return text;
+  //   for (let i = 0; i < 300; i++) {
+  //     const exists = itemPrices.find(item => item.url == this.tradeItem[i].url);
+  //     if (exists) {
+  //       this.tradeItem[i] = exists;
+  //       this.current += 1;
+  //       continue;
+  //     }
+  //     this.currentItem = this.tradeItem[i].name;
+  //     let response = await firstValueFrom(this.service.getMarketPrices(this.tradeItem[i].url));
+  //     if (!response?.data) {
+  //       this.current += 1;
+  //       await new Promise(resolve => setTimeout(resolve, 500));
+  //       continue;
+  //     }
+
+  //     response.data = response.data.filter(i => i.visible == true && i.type == 'sell');
+
+  //     const list = response.data.sort((a, b) => { return +a.platinum - +b.platinum });
+  //     this.tradeItem[i].lowest = +list[0].platinum;//Get lowest
+  //     this.tradeItem[i].higher = +list[list.length - 1].platinum;//Get higher
+
+  //     let plSum = 0;
+  //     let itemQtd = 0;
+  //     let occurrences = [];
+  //     //Get avarage price
+  //     for (const item of list) {
+  //       itemQtd = itemQtd + +item.quantity;
+  //       plSum = plSum + (+item.platinum * +item.quantity);
+
+  //       occurrences[+item.platinum] = (occurrences[+item.platinum] || 0) + +item.quantity;
+  //     }
+  //     this.tradeItem[i].average = +(plSum / itemQtd).toFixed(0);
+
+  //     let biggest = 0;
+  //     let most_frequent = '';
+  //     //Search for most frequent item price
+  //     for (const valor in occurrences) {
+  //       if (occurrences[valor] > biggest) {
+  //         biggest = occurrences[valor];
+  //         most_frequent = valor;
+  //       }
+  //     }
+  //     this.tradeItem[i].most_frequent = +most_frequent;
+
+  //     await new Promise(resolve => setTimeout(resolve, 500));
+
+  //     this.current += 1;
+  //   }
+
+  //   this.downloadText();
+  // }
+
+  downloadText() {
+    const list = this.tradeItem.filter(i => i.average > 0 && i.higher > 0 && i.lowest > 0);
+    const data = JSON.stringify(list, null, 2);
+    const blob = new Blob([data], { type: 'text/plain' });
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'trade_items.txt';
+    document.body.appendChild(a);
+    a.click();
+    window.URL.revokeObjectURL(url);
+    document.body.removeChild(a);
   }
 
   sort(field: string) {
-    this.tradeItem.sort((a, b) => {
+    this.myList.sort((a, b) => {
       if (!isNaN(+a[field])) return (this.lastSort == field ? +b[field] - +a[field] : +a[field] - +b[field]);
 
       return (this.lastSort == field ? b[field].localeCompare(a[field]) : a[field].localeCompare(b[field]));
