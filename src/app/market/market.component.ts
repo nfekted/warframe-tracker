8349759685045de36a5c15367e1defc5d502e48e
Leandro Souto de Paula <le-soutopaula@hotmail.com>
2025-07-09 22:11:23 -0300

    Best relics

--- src/app/market/market.component.ts
+++ src/app/market/market.component.ts
@@ -6,6 +6,7 @@ import { PythonToolComponent } from '../python-tool/python-tool.component';
 import itemEnJson from '../../shared/jsons/tradeable/tradeEn.json';
 import itemPtJson from '../../shared/jsons/tradeable/tradePt.json'
 import itemPrices from '../../shared/jsons/tradeable/tradePrices.json';
+import relicPrices from '../../shared/jsons/tradeable/relics.json';
 import { Util } from '../../shared/utils/util';
 
 @Component({
@@ -41,6 +42,10 @@ export class MarketComponent {
 
   maxRender: number = 100;
 
+  relics: { name: string, worst: number, worst_name: string, worst_url: string, best: number, best_name: string, best_url: string, expected: number }[] = [];
+  maxRelic: number = 100;
+  relicFilter: string;
+
   constructor() { }
 
   ngOnInit(): void {
@@ -54,6 +59,32 @@ export class MarketComponent {
   loadPrices() {
     this.tradeItem = itemPrices;
     this.tradeItem.sort((a, b) => { return b.most_frequent - a.most_frequent });
+    this.loadRelics();
+  }
+
+  loadRelics() {
+    this.maxRelic = 100;
+    this.relics = relicPrices;
+
+    switch (this.relicFilter) {
+      case 'Lith':
+        this.relics = this.relics.filter(r => r.name.includes('Lith '));
+        break;
+      case 'Meso':
+        this.relics = this.relics.filter(r => r.name.includes('Meso '));
+        break;
+      case 'Neo':
+        this.relics = this.relics.filter(r => r.name.includes('Neo '));
+        break;
+      case 'Axi':
+        this.relics = this.relics.filter(r => r.name.includes('Axi '));
+        break;
+      case 'Requiem':
+        this.relics = this.relics.filter(r => r.name.includes('Requiem '));
+        break;
+    }
+
+    this.relics.sort((a, b) => { return b.expected - a.expected });
   }
 
   updateMyList() {
@@ -125,8 +156,74 @@ export class MarketComponent {
   //Dev only codes
   scan() {
     // this.formatPriceJson();
+    // this.formatRelics();
     // this.getPrices();
   }
+
+  // formatRelics() {
+  //   const lines = this.relicsData.split('\n');
+  //   this.relics = [];
+
+  //   let relic = { name: '', worst: 0, worst_name: '', best: 0, best_name: '', expected: 0, worst_url: '', best_url: '' };
+  //   for (let i = 0; i < lines.length; i++) {
+  //     const line = lines[i];
+  //     if (line == '') continue;
+  //     if (line.includes('Relic (Intact)') || line.includes('Relic (Exceptional)') || line.includes('Relic (Flawless)')) {
+  //       i += 6;
+  //       continue;
+  //     }
+
+  //     if (line.includes('Relic (Radiant)')) {
+  //       relic = { name: line.split(',')[0], worst: 0, best: 0, expected: 0, worst_name: '', best_name: '', worst_url: '', best_url: '' };
+
+  //       const drops = [
+  //         lines[i + 1].split(','),
+  //         lines[i + 2].split(','),
+  //         lines[i + 3].split(','),
+  //         lines[i + 4].split(','),
+  //         lines[i + 5].split(','),
+  //         lines[i + 6].split(',')
+  //       ];
+
+  //       console.log(i + ` de ` + lines.length)
+
+  //       let items = [];
+  //       for (let j = 0; j < 6; j++) {
+  //         drops[j][1] = drops[j][1].split('(')[1].split('%')[0];
+  //         items.push(this.tradeItem.find(i => i.name.toLocaleLowerCase() == drops[j][0].toLocaleLowerCase()));
+  //       }
+
+  //       items = items.filter(i => i != undefined);
+  //       items.sort((a, b) => a.most_frequent - b.most_frequent);
+
+  //       let expected: number = 0;
+  //       for (let j = 0; j < items.length; j++) {
+  //         expected += items[j].most_frequent * (+drops[j][1] / 100);
+  //       }
+  //       relic.worst = items[0].most_frequent;
+  //       relic.worst_name = items[0].name;
+  //       relic.worst_url = items[0].url;
+  //       relic.best = items[items.length - 1].most_frequent;
+  //       relic.best_name = items[items.length - 1].name;
+  //       relic.best_url = items[items.length - 1].url;
+  //       relic.expected = +expected.toFixed(0);
+
+  //       this.relics.push(relic);
+  //     }
+  //   }
+
+  //   const data = JSON.stringify(this.relics, null, 2);
+  //   const blob = new Blob([data], { type: 'text/plain' });
+  //   const url = window.URL.createObjectURL(blob);
+  //   const a = document.createElement('a');
+  //   a.href = url;
+  //   a.download = 'relics.txt';
+  //   document.body.appendChild(a);
+  //   a.click();
+  //   window.URL.revokeObjectURL(url);
+  //   document.body.removeChild(a);
+
+  // }
 
   // formatPriceJson() {
   //   for (const key of Object.keys(itemPrices)) {
