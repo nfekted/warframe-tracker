6688c0c96e5a98f49333d74848416ea5b8465d61
Leandro Souto de Paula <le-soutopaula@hotmail.com>
2025-11-26 23:39:33 -0300

    Update 40.0

--- src/app/transloco-loader.ts
+++ src/app/transloco-loader.ts
@@ -4,9 +4,10 @@ import { HttpClient } from "@angular/common/http";
 
 @Injectable({ providedIn: 'root' })
 export class TranslocoHttpLoader implements TranslocoLoader {
-    private http = inject(HttpClient);
+  private http = inject(HttpClient);
 
-    getTranslation(lang: string) {
-  return this.http.get<Translation>(`/warframe-tracker/assets/i18n/${lang}.json`);
-    }
+  getTranslation(lang: string) {
+    // return this.http.get<Translation>(`/assets/i18n/${lang}.json`);
+    return this.http.get<Translation>(`/warframe-tracker/assets/i18n/${lang}.json`);
+  }
 }
